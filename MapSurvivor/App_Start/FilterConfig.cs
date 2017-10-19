fe398e1fa1995270951e66b095b190ce6375d76b
AQMacgregor <A.Q.Macgregor@gmail.com>
2017-10-19 11:40:14 +0100

    UI Base

---
+++ MapSurvivor/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MapSurvivor
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
